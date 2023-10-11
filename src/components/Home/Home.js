91968a4090f384752ddaf86c6925e65834997543
thefullstackboy <66158784+thefullstackboy@users.noreply.github.com>
2023-10-11 17:37:47 +0530

    price

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,126 @@
 import React from 'react'
+import "../Home/Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-md-2 col-xl-2 col-lg-2 col-sm-12'>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+          <p>text text text text</p>
+
+        </div>
+        <div className='justify-content-center col-md-8 col-xl-8 col-lg-8 col-sm-12'>
+        <div class="card mb-3 cardwidth">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN_Sp_aFk7FISfZRUl4jZ8FNn_9go0iqwsHA&usqp=CAU" class="img-fluid rounded-start imgesize" alt="..."/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title text-white">Card title"main title"</h3>
+        <h5 class="card-title text-white">Card title"sub title"</h5>
+        <p class="card-text text-white">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h4 class="card-text text-white">Price 499</h4>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3 cardwidth">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN_Sp_aFk7FISfZRUl4jZ8FNn_9go0iqwsHA&usqp=CAU" class="img-fluid rounded-start imgesize" alt="..."/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title text-white">Card title"main title"</h3>
+        <h5 class="card-title text-white">Card title"sub title"</h5>
+        <p class="card-text text-white">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h4 class="card-text text-white">Price 499</h4>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3 cardwidth">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN_Sp_aFk7FISfZRUl4jZ8FNn_9go0iqwsHA&usqp=CAU" class="img-fluid rounded-start imgesize" alt="..."/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title text-white">Card title"main title"</h3>
+        <h5 class="card-title text-white">Card title"sub title"</h5>
+        <p class="card-text text-white">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h4 class="card-text text-white">Price 499</h4>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3 cardwidth">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN_Sp_aFk7FISfZRUl4jZ8FNn_9go0iqwsHA&usqp=CAU" class="img-fluid rounded-start imgesize" alt="..."/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title text-white">Card title"main title"</h3>
+        <h5 class="card-title text-white">Card title"sub title"</h5>
+        <p class="card-text text-white">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h4 class="card-text text-white">Price 499</h4>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+        <i class="star"></i>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
