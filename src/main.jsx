5ffa4dbf4f366ddccf4fbd71f05b51520ca4dcda
Chandan Prasad <chandanp@JLMP8920.local>
2023-10-15 14:53:05 +0530

    reordering of URL Endpoints

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 module.exports = {
     URL:{
-        SIGN_IN:"/sign-in",
-        FORGOT_PASSWORD:"/forgot-password",
-        SIGN_UP:"/sign-up",
         HOME:"/",
+        SIGN_IN:"/sign-in",
+        SIGN_UP:"/sign-up",
+        FORGOT_PASSWORD:"/forgot-password",
         DETAILS_PAGE:"/details-page"
     }
 }
